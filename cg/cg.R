bc5ff742ddd2ad35684c4c70056f71d8fd51abd4
Arshad Azad <85057289+azadecon@users.noreply.github.com>
2023-12-23 18:30:38 +0530

    Create cg.R

---
+++ cg/cg.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(janitor)
+library(readxl)
+library(rvest)
+library(data.table)
+library(httr)
+
+rm(const)
+rm(url)
+for (const in c(1:90)) {
+  headers = c(
+    'authority' = 'results.eci.gov.in',
+    'accept' = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language' = 'en-US,en;q=0.9',
+    'cookie' = 'RT="z=1&dm=results.eci.gov.in&si=981bc7bf-7ba3-43b0-b725-b539e92aef8b&ss=lqh10mbo&sl=7&tt=2c1&bcn=%2F%2F684d0d48.akstat.io%2F&ld=2td8&nu=31utebg0&cl=2x6n&ul=2x6s"',
+    'referer' = 'https://results.eci.gov.in/AcResultGenDecNew2023/candidateswise-S2053.htm',
+    'sec-ch-ua' = '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile' = '?1',
+    'sec-ch-ua-platform' = '"Android"',
+    'sec-fetch-dest' = 'document',
+    'sec-fetch-mode' = 'navigate',
+    'sec-fetch-site' = 'same-origin',
+    'sec-fetch-user' = '?1',
+    'upgrade-insecure-requests' = '1',
+    'user-agent' = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36'
+  )
+  url <- paste0("https://results.eci.gov.in/AcResultGenDecNew2023/ConstituencywiseS26", const, ".htm")
+  res <- VERB("GET", url = url, add_headers(headers))
+  
+  df <- read_html(res) |> html_table() |> as.data.frame() |> clean_names()
+  df$constituency <- paste0(const)
+  
+  colnames(df) <- c("s_n",
+                    "candidate",
+                    "party",
+                    "evm_votes",
+                    "postal_votes",
+                    "total_votes", 
+                    "perc_of_votes",
+                    "constituency"
+  )
+  
+  writexl::write_xlsx(df, paste0("./cg/", const, ".xlsx"))
+  
+}
+
+
+
+
+
